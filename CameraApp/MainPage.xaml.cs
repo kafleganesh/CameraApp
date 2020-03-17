670dd5cd3c941c4d0c2fc1d1528c0ee848e8c5c6
kafleganesh <kafleganesh1235@gmail.com>
2020-03-17 18:20:40 +0100

    First protype of CameraApp

---
+++ CameraApp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace CameraApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void loadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported && !CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+            else
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Images",
+                    Name = DateTime.Now +"-test.jpg"
+                    
+                });
+                if (file == null)
+                    return;
+                await DisplayAlert("file path", file.Path, "ok");
+
+                MyImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+
+        void Cancel_Clicked(System.Object sender, System.EventArgs e)
+        {
+            return;
+        }
+    }
+}
+
+
